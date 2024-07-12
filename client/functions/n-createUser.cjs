5993672cb245150f98603f4f44985a509f77f59a
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-12 13:42:58 -0700

    see what sticks

--- client/functions/n-createUser.cjs
+++ client/functions/n-createUser.cjs
@@ -1,49 +1,56 @@
 // n-createUser.cjs
 
-// Import User and Cart models
 import User from "../../server/models/User.js";
 import Cart from "../../server/models/Cart.js";
 import bcrypt from "bcrypt";
+import passwordRegex from "../../utils/validation.mjs";
 
 // Function to create a user
 const createUser = async (userData) => {
   try {
-    // Validate the password format using regex
-    const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+}{"':;?/>.<,]).{8,50}$/;
-
     if (!passwordRegex.test(userData.password)) {
       throw new Error("Password does not meet complexity requirements");
     }
 
-    // Hash the password using bcrypt
     const hashedPassword = await bcrypt.hash(userData.password, 10);
 
-    // Create a new user with the hashed password
     const newUser = await User.create({
       ...userData,
       password: hashedPassword,
     });
 
-    // Log a message indicating that the user has been created
     console.log(`User created with ID ${newUser.id}`);
 
-    // Create a cart for the new user
     const newCart = await Cart.create({
       userId: newUser.id,
     });
 
-    // Log a message indicating that a cart has been created for the user
     console.log(
       `Cart created with ID ${newCart.id} for user with ID ${newUser.id}`
     );
 
     return newUser;
   } catch (error) {
-    // Log any errors that occur during user creation
     console.error("Error creating user:", error);
     throw error;
   }
 };
 
-export default createUser;
+// Netlify function handler
+exports.handler = async (event) => {
+  try {
+    const userData = JSON.parse(event.body);
+
+    const newUser = await createUser(userData);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(newUser),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
